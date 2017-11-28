61caf2b04c60b0ec3d1d2da3f4ffd40149e0df17
sudhir <sudhir.k@tact.co.in>
2017-11-29 02:17:48 +0530

    initial commit

---
+++ app/src/main/java/com/example/sudhir/myfirstapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.sudhir.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // broadcast a custom intent.
+    public void broadcastIntent(View view){
+        Intent intent = new Intent();
+        intent.setAction("com.example.sudhir.myfirstapp.CUSTOM_INTENT");
+        sendBroadcast(intent);
+    }
+}
---
+++ app/src/main/java/com/example/sudhir/myfirstapp/MyReceiver.java
@@ -0,0 +1,16 @@
+package com.example.sudhir.myfirstapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by sudhir on 13-06-2017.
+ */
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
+    }
+}
